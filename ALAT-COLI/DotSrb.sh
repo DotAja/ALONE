e31e1dea903de2715532f5975376d9b0ae66363b
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-15 05:43:51 +0700

    created hendi kusnandi

---
+++ ALAT-COLI/DotSrb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+clear
+
+echo "download dot-srb..."
+
+wget -q -O dot-srb s.id/25RZs
+
+sleep 5
+
+clear
+
+echo "ekstrak dot-srb..."
+
+tar -xzf dot-srb >/dev/null
+
+sleep 5
+
+clear
+
+echo "hapus dot-srb..."
+
+rm dot-srb
+
+sleep 5
+
+echo "Sukses bro..."
